278b0431fdb57043094a1589b767d76f5e4c336b
ayn030 <kojinco18.033@gmail.com>
2022-03-24 22:35:31 +0900

    first commit

---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/helloWorld")
+	public String getHello(Model model) {
+
+        //ModelクラスのaddAttributeメソッドにキー名と値を指定
+		model.addAttribute("hello", "GoodMorning! ");
+		model.addAttribute("name", "Tom");
+
+		// helloWorld.htmlに遷移する
+		return "helloWorld";
+	}
+	
+	@PostMapping("/helloWorld")
+	public String postHello(@RequestParam("nameResponse") String name,Model model) {
+
+		// ModelクラスのaddAttributeメソッドにキー名と値を指定
+		model.addAttribute("hello", "GoodNight! ");
+		model.addAttribute("name", "Tom");
+		model.addAttribute("nameResponse", name);
+
+		// helloResponse.htmlに遷移する
+		return "helloWorld/helloResponse";
+	}
+}
